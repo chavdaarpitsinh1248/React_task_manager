32d13cfbc747023c67fa826b795b2efe2feedebc
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-11-27 17:11:05 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import AddTask from "./components/AddTask";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (text) => {
+    const newTask = {
+      id: Date.now(),
+      text,
+      completed: false,
+    };
+
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div>
       <Header />
-      <h1>Task Manager</h1>
+      <AddTask onAdd={addTask} />
+
+      <h3>Your Tasks:</h3>
+      <pre>{JSON.stringify(tasks, null, 2)}</pre>
     </div>
   );
 }

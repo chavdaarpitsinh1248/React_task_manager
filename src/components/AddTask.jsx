e4b3e714ef9809d5ce881127333612b3405e4930
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-11-27 19:22:58 +0530

    Add files via upload

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -18,7 +18,7 @@ function AddTask({ onAdd }) {
             placeholder="Enter a task..."
             value={text}
             onChange={(e) => setText(e.target.value)}
-            className="flex-grow border border-gray-300 rounded-lg px-3 py-2 outline-none focus:ring-2 focus:ring-blue-400"
+            className="flex-grow border border-gray-300 dark:border-gray-600 rounded-lg px-3 py-2 outline-none focus:ring-2 focus:ring-blue-400 bg-white dark:bg-gray-700 text-gray-800 dark:text-gray-200"
             />
             <button
                 type="submit"

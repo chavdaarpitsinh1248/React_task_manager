cb41e9097b1852d26dad658688947f0e38afc467
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-11-27 17:38:35 +0530

    Add files via upload

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,6 +1,6 @@
 import TaskItem from "./TaskItem";
 
-function TaskList({ tasks }) {
+function TaskList({ tasks, onToggle, onDelete }) {
     if (tasks.length === 0) {
         return <p>No tasks yet.</p>;
     }
@@ -8,7 +8,12 @@ function TaskList({ tasks }) {
     return (
         <ul>
             {tasks.map((task) => (
-                <TaskItem key={task.id} task={task} />
+                <TaskItem 
+                    key={task.id} 
+                    task={task}
+                    onToggle={onToggle}
+                    onDelete={onDelete}
+                 />
             ))}
         </ul>
     );

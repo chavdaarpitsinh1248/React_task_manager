a09d54f20e99d2e1517e39ec722e242152cffc28
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-11-27 17:36:46 +0530

    Add files via upload

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,7 +1,17 @@
-function TaskItem({ task }) {
+function TaskItem({ task, onToggle, onDelete }) {
     return (
         <li>
-            <span>{task.text}</span>
+            <input 
+                type="checkbox"
+                checked={task.completed}
+                onChange={() => onToggle(task.id)}
+            />
+
+            <span style={{ textDecoration: task.completed ? "line-through" : "none" }}>
+                {task.text}
+            </span>
+
+            <button onClick={() => onDelete(task.id)}>Delete</button>
         </li>
     );
 }
